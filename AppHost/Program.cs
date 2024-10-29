0da7db364411d4ca50804bf648bb719a253c7a22
Lamparter <notlamparter@outlook.com>
2024-10-29 08:47:54 +0000

    Remove ASP.NET website

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -4,11 +4,6 @@ var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.Republic_ApiService>("apiservice");
 
-builder.AddProject<Projects.Republic_Web>("webfrontend")
-	.WithExternalHttpEndpoints()
-	.WithReference(cache)
-	.WithReference(apiService);
-
 builder.AddProject<Projects.Republic_Judiciary>("court");
 
 builder.AddProject<Projects.Republic_Portals>("portals");
