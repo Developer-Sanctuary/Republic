3cbd382c2d0ad84cf8a37987c036c3dff51608b3
Lamparter <notlamparter@outlook.com>
2024-10-30 12:56:41 +0000

    Unify command interaction handlers by interface

---
+++ Shared/IUnionReusedMetadataCommandsBase.cs
@@ -0,0 +1,16 @@
+﻿using Republic.Embeds;
+using Discord;
+
+namespace Republic
+{
+	public interface IUnionReusedMetadataCommandsBase
+	{
+		Task HandlePingAsync();
+		Task HandleHelpAsync();
+
+		string HelpReference { get; }
+		Embed HelpEmbed { get; }
+		Embed PingEmbed { get; }
+	}
+
+}
