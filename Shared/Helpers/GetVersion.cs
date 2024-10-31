66914f59237b86f3add0e058185c9792a2678d6d
Lamparter <notlamparter@outlook.com>
2024-10-31 09:17:46 +0000

    Add helper to get version

---
+++ Shared/Helpers/GetVersion.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Diagnostics;
+
+namespace Republic.Helpers
+{
+	public class GetVersion
+	{
+		public static int GetApiVersion()
+		{
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+			return fileVersionInfo.FileMajorPart;
+		}
+	}
+}
